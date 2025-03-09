7b4dcbfd9619967de1df60db863a44be46847ee7
Jeremy Oweh <141310678+dinduoweh@users.noreply.github.com>
2025-03-09 19:57:51 -0400

    wrote and successfully ran tests for int getArea

--- CSCN71020_A2 - Copy/UnitTest/UnitTest.cpp
+++ CSCN71020_A2 - Copy/UnitTest/UnitTest.cpp
@@ -8,17 +8,28 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest
 {
-	TEST_CLASS(testgetperimeter)
+	TEST_CLASS(UNITTESTS)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Test_getPerimeter)
 		{
 			int length = 8;
 			int width = 3;
 			int expectedvalue = 2 * (length + width);
 
 			int actualvalue = getPerimeter(&length, &width);
+
+			Assert::AreEqual(expectedvalue, actualvalue);
+		}
+
+		TEST_METHOD(Test_getArea)
+		{
+			int length = 6;
+			int width = 3;
+			int expectedvalue = length * width;
+
+			int actualvalue = getArea(&length, &width);
 
 			Assert::AreEqual(expectedvalue, actualvalue);
 		}
